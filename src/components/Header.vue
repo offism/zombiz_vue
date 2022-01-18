65118835c393fba67ba85940b106388f44525f14
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-01-18 20:04:25 +0300

    1day 1

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    <h1>Salom</h1>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
