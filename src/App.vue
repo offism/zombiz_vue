f368b56c72812c78e0d658c623019f50c08c3006
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-02-11 22:16:53 +0300

    11.02d-c6

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <Header />
   <ConsultingHead />
+  <WhatWeDo />
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import ConsultingHead from "./components/ConsultingHeadPart/ConsultingHead.vue";
+import WhatWeDo from "./components/ConsultingHeadPart/WhatWeDo.vue";
 
 export default {
   name: "App",
   components: {
     Header,
     ConsultingHead,
+    WhatWeDo,
   },
 };
 </script>
---
+++ src/components/WhatWeDo.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="WhatWeDo"></section>
+</template>
+
+<script>
+export default {
+  name: "WhatWeDo",
+};
+</script>
+
+<style scoped></style>
