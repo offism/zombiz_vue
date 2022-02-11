43994a400e39a8555b1cf99acd1bed04c77f7095
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-02-11 23:00:41 +0300

    11.02d-c8

--- src/components/WhatWeDo.vue
+++ src/components/WhatWeDo.vue
@@ -12,21 +12,50 @@
       <div class="wwd-footer">
         <div class="wwd__pacet">
           <img
+            class="img-fluid"
             src="./../../public/images/home/busisness_grow.png"
             alt="Business Grow img"
           />
+          <h4 class="wwd-pacet__title">
+            <a href="#"> Business Consultancy </a>
+          </h4>
+
+          <small class="wwd-pacet__text"
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo et
+            veniam rerum impedit sequi laborum error expedita nostrum quis
+            exercitationem!</small
+          >
         </div>
         <div class="wwd__pacet">
           <img
+            class="img-fluid"
             src="./../../public/images/home/consultency.png"
             alt="Consultency img"
           />
+          <h4 class="wwd-pacet__title">
+            <a href="#"> Business Consultancy </a>
+          </h4>
+
+          <small class="wwd-pacet__text"
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo et
+            veniam rerum impedit sequi laborum error expedita nostrum quis
+            exercitationem!</small
+          >
         </div>
         <div class="wwd__pacet">
           <img
+            class="img-fluid"
             src="./../../public/images/home/support-logo.png"
             alt="Support Logo img"
           />
+          <h4 class="wwd-pacet__title">
+            <a href="#"> Great Support </a>
+          </h4>
+          <small class="wwd-pacet__text"
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo et
+            veniam rerum impedit sequi laborum error expedita nostrum quis
+            exercitationem!</small
+          >
         </div>
       </div>
     </div>
@@ -62,6 +91,7 @@ export default {
   max-width: 1000 px;
   margin: 0 auto;
   margin-top: 32 px;
+  margin-bottom: 75px;
   line-height: 1.8;
 }
 .wwd-footer {
@@ -72,11 +102,37 @@ export default {
 }
 .wwd__pacet {
   margin: 15px 20px 0;
-  height: 30%;
+  min-height: 30%;
   width: 30%;
-  padding: 30px 20px;
+  padding: 70px 30px;
   box-shadow: 0 0 5px rgb(35 37 41 / 10%);
   overflow-x: hidden;
+  opacity: 1;
+  transition: 0.3s ease all;
+}
+.wwd__pacet img {
+  margin-bottom: 20px;
+}
+.wwd-pacet__title a {
+  margin: 20px 0;
+  color: #44454f;
+  font-size: 20px;
+  font-family: "Playfair Display", serif;
+  transition: 0.3s ease all;
+}
+.wwd-pacet__title a:hover {
+  font-weight: 600;
+  text-decoration: overline, underline;
+}
+.wwd-pacet__text {
+  color: #888;
+  font-size: 16px;
+  font-family: "Poppins", sans-serif;
+  max-width: 306 px;
+  margin: 0 auto;
+}
+.wwd__pacet:hover {
+  box-shadow: 0 5px 20px rgb(112 134 230 / 20%);
   opacity: 1;
 }
 </style>
