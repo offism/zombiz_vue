0f3abe60ed2909d6aa2cde1bdc9c2486a3de3b00
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-02-12 23:29:51 +0300

    12.02d-c2

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -8,9 +8,10 @@
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
             in voluptas explicabo molestiae provident accusamus culpa beatae
             quaerat dolor vel. Illo dolorem neque expedita itaque nisi quo
-            placeat atque nihil.
+            placeat atque nihil. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit.
           </p>
-          <a href="#" class="au-box__link">learn more</a>
+          <a href="#" class="au-box__link btn">learn more</a>
         </div>
         <div class="col-6 au-img">
           <img
@@ -37,5 +38,50 @@ export default {
 .about-us__box {
   display: flex;
   align-items: center;
+  text-align: left;
+}
+.au-text__title {
+  font-family: "Playfair Display", serif;
+  color: #4d4e54;
+  font-size: 30px;
+  font-weight: 700;
+  margin-bottom: 20px;
+}
+.au-box__text {
+  color: #888;
+  font-size: 16px;
+  font-family: "Poppins", sans-serif;
+  line-height: 1.7;
+  width: 480px;
+  margin-bottom: 20px;
+}
+.au-box__link {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #7370d8;
+  width: 185px;
+  height: 52px;
+  line-height: 50px;
+  white-space: nowrap;
+  text-align: center;
+  color: #fff;
+  text-transform: uppercase;
+  transition: 0.3s ease all;
+}
+.au-box__link:hover {
+  background: #fff;
+  color: #7370d8;
+  border: 1px solid #7370d8;
+}
+.au-img::before {
+  position: absolute;
+  content: "";
+  top: 55px;
+  left: -23px;
+  border: 3px solid #ebecfb;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
 }
 </style>
