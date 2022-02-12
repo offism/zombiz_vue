641f915bb52831579fac6e2ecdc1e11e975e42e9
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-02-13 00:29:55 +0300

    13.02d-c1

--- src/components/FirstCounter.vue
+++ src/components/FirstCounter.vue
@@ -1,7 +1,30 @@
 <template>
   <section class="FirstCounter">
     <div class="container">
-      <div class="first-counter__box"></div>
+      <div class="first-counter__box">
+        <div class="row">
+          <div class="col-3 fc__pacet">
+            <div class="row">
+              <div class="fc-pacet-img__box col-6">
+                <img
+                  src="./../../public/images/counter/counter1.png"
+                  alt=""
+                  class="fc-pacet__img"
+                />
+              </div>
+              <div class="fc-pacet-text__box col-6">
+                <div class="row">
+                  <div class="counter">0</div>
+                  <p class="fc-pacet__title">Days Worked</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-3 fc__pacet"></div>
+          <div class="col-3 fc__pacet"></div>
+          <div class="col-3 fc__pacet"></div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -21,8 +44,34 @@ export default {
   padding: 98px 0 81px;
   min-height: 40vh;
 }
+.FirstCounter::before {
+  position: absolute;
+  content: "";
+  background: rgba(115, 112, 216, 0.8);
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+}
 .first-counter__box {
-  display: flex;
-  flex-direction: column;
+  display: block;
+  margin: 0 50px;
+  position: relative;
+}
+.fc-pacet__img {
+  height: 60px;
+}
+.counter {
+  color: #fff;
+  font-size: 32px;
+  font-family: "Poppins", sans-serif;
+}
+.fc-pacet__title {
+  color: #fff;
+  font-size: 20px;
+  text-transform: capitalize;
+  font-family: "Poppins", sans-serif;
+  line-height: 1.5;
+  padding: 0;
 }
 </style>
