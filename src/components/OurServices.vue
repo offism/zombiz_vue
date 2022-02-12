72ac4fe6afddb74fbc7cdede57b7b111a90a7b2a
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-02-12 23:50:47 +0300

    12.02d-c4

--- src/components/OurServices.vue
+++ src/components/OurServices.vue
@@ -14,13 +14,15 @@
         <div class="row">
           <div class="col-4 os__pacet">
             <img src="./../../public/images/service/service1.png" alt="" />
-            <h4 class="os-pacet__title">Business Planning</h4>
-            <small
+            <h4 class="os-pacet__title">
+              <a href="#" class="btn"> Business Planning </a>
+            </h4>
+            <small class="os-pacet__text"
               >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus
               repellat nobis excepturi eum quia perferendis! Labore autem
               similique deserunt fuga.</small
             >
-            <a href="#" class="btn">Learn More</a>
+            <a href="#" class="os-pacet__link btn">Learn More</a>
           </div>
           <div class="col-4 os__pacet"></div>
           <div class="col-4 os__pacet"></div>
@@ -59,9 +61,32 @@ export default {
   color: #888;
   font-size: 16px;
   font-family: "Poppins", sans-serif;
-  max-width: 1000 px;
-  margin: 0 auto;
-  margin-top: 32 px;
+  max-width: 1000px;
+  margin: 32px auto 55px;
   line-height: 1.8;
+}
+.os-pacet__title a {
+  color: #44454f;
+  font-size: 24px;
+  font-family: "Playfair Display", serif;
+  padding: 47px 0 25px;
+  transition: 0.3s ease all;
+}
+.os-pacet__title a:hover {
+  font-weight: 600;
+}
+.os-pacet__text {
+  color: #756c6a;
+  font-size: 16px;
+  font-family: "Poppins", sans-serif;
+  line-height: 1.67;
+}
+.os-pacet__link {
+  color: #7370d8;
+  font-size: 16px;
+  font-family: "Poppins", sans-serif;
+  text-transform: capitalize;
+  margin-top: 12 px;
+  margin-bottom: 15 px;
 }
 </style>
